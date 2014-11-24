3463be2158c77ba42e2a194c674d3828481c2f7c
Hakim Cassimally <hakim@mysociety.org>
2014-11-24 12:09:03 +0000

    Fix import of FMS reports

--- mot_fms_project/mot_fms/management/commands/import_fms_postcodes.py
+++ mot_fms_project/mot_fms/management/commands/import_fms_postcodes.py
@@ -12,5 +12,23 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with open('data/fms-postcodes.csv', 'r') as f:
             reader = csv.reader(f)
-            for row in reader:
-                print row
+
+            # ['334881', 'GU', '51.172109650377', '-0.46403161059507']
+            reader.next() # discard header row
+
+            count = 0
+
+            for (fms_report_id, code, latitude, longitude) in reader:
+                (postcode, _) = Postcode.objects.get_or_create( code=code );
+                FMSReport.objects.create(
+                    fms_report_id=fms_report_id,
+                    latitude=latitude,
+                    longitude=longitude,
+                    postcode=postcode
+                )
+                count += 1
+
+            print "Imported %d records" % count
+                    
+
+
