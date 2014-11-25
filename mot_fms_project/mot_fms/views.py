223b9fe31c0dba865e2793c84d835138afdd8fc5
lizconlan <liz@mysociety.org>
2014-11-25 23:43:51 +0000

    Slightly more complicated vehicle views

--- mot_fms_project/mot_fms/views.py
+++ mot_fms_project/mot_fms/views.py
@@ -1,7 +1,8 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-from mot_fms.models import Postcode, VehicleMake
+from mot_fms.models import Postcode, VehicleMake, Vehicle
+from django.db.models import Count
 
 class PostcodeListView(ListView):
     model = Postcode
@@ -10,8 +11,15 @@ class PostcodeDetailView(DetailView):
     model = Postcode
 
 class VehicleMakeListView(ListView):
-    queryset = VehicleMake.objects.order_by('make')
+    # there's probably a neater way of doing this but I haven't been able to find it
+    queryset = VehicleMake.objects.annotate(num_vehicles=Count('vehicle')).filter(num_vehicles__gt=0).order_by('make')
     context_object_name = 'make_list'
 
 class VehicleMakeDetailView(DetailView):
     model = VehicleMake
+
+    def get_context_data(self, **kwargs):
+        context = super(VehicleMakeDetailView, self).get_context_data(**kwargs)
+        context['count'] = Vehicle.objects.filter(make_id=self.get_object().make_id).count
+        context['total_vehicles'] = Vehicle.objects.count
+        return context
